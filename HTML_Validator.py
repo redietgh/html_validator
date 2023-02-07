cbdf48db06146bd85daf2e937361d6e06a6c77fd
rkga2021 <rkga2021@mymail.pomona.edu>
2023-02-07 00:33:25 -0800

    take 2

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 import re
 
+
 def validate_html(html):
     '''
     This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
